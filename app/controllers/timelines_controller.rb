e0013cd16652eedfb6a038056bd3cab1d1cfe7c7
Ayane Matsumoto <sayanee100.go@gmail.com>
2016-06-23 01:44:34 +0000

    add like/unlike function

--- app/controllers/timelines_controller.rb
+++ app/controllers/timelines_controller.rb
@@ -5,14 +5,14 @@ class TimelinesController < ApplicationController
     @my_twitter ||= MyTwitter.new
     @articles = Array.new(11){ Array.new(3) }
     i = 0
-    @my_twitter.client.search("fashion", result_type: "recent").take(10).each do |tweet|
+    @my_twitter.client.search("OOTD", result_type: "recent").take(30).each do |tweet|
       #画像付きだったら画像URIをとってくる。
       if tweet.media? then
         tweet.media.each do |media|
-          @articles[i] = [tweet.id,tweet.text,media.media_uri,tweet.created_at]
+          @articles[i] = [tweet.id,tweet.text,media.media_uri,tweet.created_at,false]
         end
       else
-        @articles[i] = [tweet.id,tweet.text,"",tweet.created_at]
+        @articles[i] = [tweet.id,tweet.text,"",tweet.created_at,false]
       end
       i=i+1
     end
@@ -20,21 +20,39 @@ class TimelinesController < ApplicationController
     #flicklrのデータ取得
     FlickRaw.api_key = 'ca40a058e5ceb04e8b41514d758350b3'
     FlickRaw.shared_secret = 'd1ad76b69c599de5'
-    word = "fashion"
-    flickr_data = flickr.photos.search(tags: word,tag_mode: "all", sort: "date-posted-desc", per_page: 1)
+    word = "OOTD"
+    flickr_data = flickr.photos.search(tags: word,tag_mode: "all", sort: "date-posted-desc", per_page: 3)
     flickr_data.each do |image|
         info = flickr.photos.getInfo :photo_id => image.id, :secret => image.secret
         posted = Time.at(info.dates.posted.to_i).to_s
         url = FlickRaw.url image
         tags = info.tags
         tag_list = tags.map{ |tag| "#{ tag }" }.join(", ")
-        @articles[i] = [image.id,info.description + tag_list,url,posted]
+        @articles[i] = [image.id,info.description + tag_list,url,posted,false]
         i=i+1
     end
     #created_atでソート
     @articles = @articles.sort {|a, b|
       b[3] <=> a[3]
     }
+    @cookies = cookies
+    cookies.each do |cookie|
+      @articles.each do |article|
+        if cookie[0] == article[0].to_s
+          article[4] = true 
+        end  
+      end
+    end
+    
   end #end_of_index
   
+  def like
+    #likeのものは解除、unlikeのものはlikeにする
+    if cookies[params[:data][0].to_s].nil?
+      cookies[params[:data][0].to_s] = [params[:data]]
+    else
+      cookies.delete params[:data][0].to_s
+    end
+    redirect_to timelines_path
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,9 @@ Rails.application.routes.draw do
   
   resources :timelines
   root 'timelines#index'
+  #get 'timelines/like/:id' => 'timelines#like', as: 'like'
+  post 'timelines/like' => 'timelines#like'
+  
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
